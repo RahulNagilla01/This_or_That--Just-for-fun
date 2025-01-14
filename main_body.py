15bcb8e59a4fcf75e6ab68e65423ff5f90cc03be
okaGeek <rahulnagilla0@gmail.com>
2025-01-15 01:41:38 +0530

    main_body.py
    Created the main body and the main structure of this project, still need to create the user interface using tkinter. I also need to test.

---
+++ main_body.py
@@ -0,0 +1,18 @@
+import random
+
+number_of_choices = int(input("Please Give the number of Choices"))
+
+Choices = []
+count = 0
+for choice in range(number_of_choices):
+  count += 1
+  choice = str(input(f"Please give your Choice {count}: "))
+  Choices.append(choice)
+
+print("Your Total number of Choices are: ", count)
+print("This is just a funny project, Use at your own risk, /n  We are not responsible for any cause that happend to you by using this automated decision maker.")
+
+def final_decision(Choices):
+  decision = Choices.choice()
+  return decision 
+
